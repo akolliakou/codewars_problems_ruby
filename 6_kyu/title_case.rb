0b507f5b2ee7478d2edddd3e4e455ae4e8d29e17
Anna Kolliakou <akolliakou@gmail.com>
2021-02-25 11:33:33 +0000

    More 6 kyu problems

---
+++ 6_kyu/title_case.rb
@@ -0,0 +1,35 @@
+# 6 kyu
+
+# A string is considered to be in title case if each word in the string is 
+# either (a) capitalised (that is, only the first letter of the word is 
+# in upper case) or (b) considered to be an exception and put entirely 
+# into lower case unless it is the first word, which is always 
+# capitalised.
+# Write a function that will convert a string into title case, 
+# given an optional list of exceptions (minor words). The list of minor 
+# words will be given as a string with each word separated by a space. 
+# Your function should ignore the case of the minor words string -- 
+# it should behave in the same way even if the case of the minor word 
+# string is changed.
+
+def title_case(title, exceptions = '')
+  title_arr = []
+  str_title = title.downcase.split
+  exceptions = exceptions.downcase.split
+
+  str_title.each_with_index do |word, idx|
+    if exceptions.include?(word) && idx > 0
+      title_arr << word.downcase
+    elsif exceptions.include?(word)
+      title_arr << word.capitalize
+    else
+      title_arr << word.capitalize
+    end
+  end
+
+    title_arr.join(" ")
+end
+
+p title_case('a clash of KINGS', 'a an the of') == 'A Clash of Kings'
+p title_case('THE WIND IN THE WILLOWS', 'The In') == 'The Wind in the Willows'
+p title_case('the quick brown fox') == 'The Quick Brown Fox'
