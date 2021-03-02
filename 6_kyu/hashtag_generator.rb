450e79cb0764e53b25ed938dfef557a4a7acdda5
Anna Kolliakou <akolliakou@gmail.com>
2021-03-02 17:47:33 +0000

    More 6_kyu exercises

---
+++ 6_kyu/hashtag_generator.rb
@@ -0,0 +1,36 @@
+# 5 kyu
+
+# The marketing team is spending way too much time typing in hashtags.
+# Let's help them with our own Hashtag Generator!
+# Here's the deal:
+# It must start with a hashtag (#).All words must have their first letter 
+# capitalized.If the final result is longer than 140 chars it must return 
+# false.If the input or the result is an empty string it must return false.
+
+# Examples
+
+# " Hello there thanks for trying my Kata" => "#HelloThereThanksForTryingMyKata"
+# "  Hello   World  "         => "#HelloWorld"
+# ""           => false
+
+def generateHashtag(str)
+  clean_str = str.squeeze(" ").split.map(&:capitalize).join(" ")
+
+  if clean_str.empty? || clean_str.length >= 140
+    false
+  else
+    clean_str.delete(" ").prepend('#')
+  end
+
+end
+
+p generateHashtag("") == false
+p generateHashtag(" " * 200) == false
+p generateHashtag("Do We have A Hashtag") == "#DoWeHaveAHashtag"
+p generateHashtag("Codewars") == "#Codewars"
+p generateHashtag("Codewars Is Nice") == "#CodewarsIsNice"
+p generateHashtag("Codewars is nice") == "#CodewarsIsNice"
+p generateHashtag("code" + " " * 140 + "wars") == "#CodeWars"
+p generateHashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat") == false
+p generateHashtag("a" * 139) == "#A" + "a" * 138
+p generateHashtag("a" * 140) == false
