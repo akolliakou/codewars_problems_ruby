adf53e7fb6eec12fa781a58a9596bb2c6db2ccc0
Anna Kolliakou <akolliakou@gmail.com>
2021-04-09 16:20:14 +0100

    More 6-kyu problems

---
+++ 6_kyu/grouping_and_counting.rb
@@ -0,0 +1,34 @@
+# 6 kyu
+
+# Your goal is to write the group_and_count method, which should receive an array as a unique 
+# parameter and return a hash. Empty or nil input must return nil instead of a hash. This hash 
+# returned must contain as keys the unique values of the array, and as values the counting of each value.
+
+# Example usage:
+# input = [1,1,2,2,2,3]
+# group_and_count(input)# == {1=>2, 2=>3, 3=>1}
+
+# The following methods were disabled:
+# Array#count
+
+# Array#length
+
+def group_and_count(arr)
+  return nil if arr == nil || arr.empty?
+  count_hsh = {}
+
+  arr.each do |int|
+    if count_hsh.include?(int)
+      count_hsh[int] += 1
+    else
+      count_hsh[int] = 1
+    end
+  end
+
+  count_hsh
+end
+
+p group_and_count([1,1,2,2,2,3]) == {1=>2, 2=>3, 3=>1}
+p group_and_count([]) == nil
+p group_and_count(nil) == nil
+p group_and_count([1, 7, 5, -1]) == {1=>1, 7=>1, 5=>1, -1=>1}
