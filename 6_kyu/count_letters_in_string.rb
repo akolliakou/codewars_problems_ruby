54ae249dd1519fdc1ae991a3f951ac3807bece11
Anna Kolliakou <akolliakou@gmail.com>
2021-04-12 07:58:25 +0100

    Update problem solution

--- 6_kyu/count_letters_in_string.rb
+++ 6_kyu/count_letters_in_string.rb
@@ -25,7 +25,23 @@ def letter_count(str)
   count_hash
 end
 
-p letter_count('arithmetics') #=> {:a=>1, :c=>1, :e=>1, :h=>1, :i=>2, :m=>1, :r=>1, :s=>1, :t=>2}
+#second approach
+
+def letter_count(str)
+  count_hash = {}
+
+  str.chars do |char|
+    if count_hash.include?(char.to_sym)
+      count_hash[char.to_sym] += 1
+    else
+      count_hash[char.to_sym] = 1
+    end
+  end
+
+  count_hash
+end
+
+p letter_count('arithmetics') == {:a=>1, :c=>1, :e=>1, :h=>1, :i=>2, :m=>1, :r=>1, :s=>1, :t=>2}
 p letter_count('codewars') == {:a=>1, :c=>1, :d=>1, :e=>1, :o=>1, :r=>1, :s=>1, :w=>1})
 p letter_count('activity') == {:a=>1, :c=>1, :i=>2, :t=>2, :v=>1, :y=>1}
 p letter_count('arithmetics') == {:a=>1, :c=>1, :e=>1, :h=>1, :i=>2, :m=>1, :r=>1, :s=>1, :t=>2})
