951e707df949ad8748e513b49e1da41abf7254d6
Anna Kolliakou <akolliakou@gmail.com>
2021-06-03 11:19:29 +0100

    More 6_kyu problems

---
+++ 6_kyu/sherlock_on_pockets.rb
@@ -0,0 +1,33 @@
+# 6 kyu
+
+# Sherlock has to find suspects on his latest case. He will use your method, dear Watson. 
+#   Suspect in this case is a person which has something not allowed in his/her pockets.
+# Allowed items are defined by array of numbers.
+# Pockets contents are defined by map entries where key is a person and value is one or few 
+# things represented by an array of numbers (can be nil or empty array if empty), example:
+# pockets = {  bob: [1], tom: [2, 5], jane: [7]} Write method which helps Sherlock to find suspects. 
+# If no suspect is found or there are no pockets (pockets == nil), the method should return nil.
+# p find_suspects(pockets, [1, 2]) == [:tom, :jane]
+# p find_suspects(pockets, [1, 7, 5, 2]) == nil
+# p find_suspects(pockets, []) == [:bob, :tom, :jane]
+# p find_suspects(pockets, [7]) == [:bob, :tom]
+
+def find_suspects(pockets, arr)
+  return nil if pockets.nil?
+  suspects = []
+
+  pockets.each do |k, v|
+    v.any? do |int|
+      suspects << k if !arr.include?(int)
+    end
+  end
+
+  return nil if suspects.empty?
+
+  suspects
+end
+
+p find_suspects(pockets, [1, 2]) == [:tom, :jane]
+p find_suspects(pockets, [1, 7, 5, 2]) == nil
+p find_suspects(pockets, []) == [:bob, :tom, :jane]
+p find_suspects(pockets, [7]) == [:bob, :tom]
